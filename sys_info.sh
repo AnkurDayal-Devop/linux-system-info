3c81d36cac6c43a49cb0476ef3e3a6221476c19e
Ankur Dayal <ankurdayal9932@gmail.com>
2026-06-17 15:40:03 +0530

    Added some lines and did some changes

--- sys_info.sh
+++ sys_info.sh
@@ -3,15 +3,18 @@
 
 echo  "====System Information===="
 echo "Hostname: $(hostname)"
+echo "------------------------"
 echo "Current User: $(whoami)"
+echo "------------------------"
 echo "Kernal Version: $(uname -r)"
-echo "Uptime:"
-uptime
-echo ::
-echo "Disk Usage:"
-df -h
+echo "------------------------"
+echo "Uptime: $(uptime)"
+echo "------------------------"
+echo "Disk Usage: $(df -h)"
+echo "------------------------"
 echo "Path of Directory: $(pwd)"
+echo "------------------------"
 echo "IP Address: $(hostname -I)"
-echo "Logged In Users:"
-who
+echo "------------------------"
+echo "Logged In Users: $(who)"
 
